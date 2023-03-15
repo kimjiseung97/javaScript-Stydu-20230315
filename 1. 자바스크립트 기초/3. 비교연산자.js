2e5a561311d930381e080fefbdebf6baa638187e
kimjiseung97 <126743072+kimjiseung97@users.noreply.github.com>
2023-03-15 15:52:41 +0900

    비교연산자

---
+++ 1. 자바스크립트 기초/3. 비교연산자.js
@@ -0,0 +1,21 @@
+var a = 5;
+var b = '5';
+
+console.log(a===b);
+console.log(a!==b);
+
+console.log('=========================================');
+
+// ==비교는 결과에측이 어려움 그렇기때문에 쓰지 않는게 좋다.
+console.log('0' ==='');
+console.log(0 === '');
+console.log('0' === 0);
+console.log(false === 'false');
+console.log(false === '0');
+
+
+//문자 대소비교
+console.log('======================');
+console.log('ace' < 'ade');
+
+
