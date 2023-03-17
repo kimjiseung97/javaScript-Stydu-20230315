8a809ce8ed7a4b47d5ff7ec427055b2ec929e0f7
kimjiseung97 <126743072+kimjiseung97@users.noreply.github.com>
2023-03-17 17:10:41 +0900

    배열 메서드

---
+++ 2. 배열/2. 배열 요소 추가, 삭제.js
@@ -0,0 +1,26 @@
+
+
+var pets = ['멍멍이','야옹이','쩝쩝이'];
+
+console.log(pets);
+
+// pets[pets.length] = '징징이';
+
+pets.push('징징이');
+pets.push('어흥이');
+var newLength = pets.push('거북이','콩콩이');
+console.log(pets);
+console.log(newLength);
+
+pets.pop();
+console.log(pets);
+
+// shift() : 배열의 맨 첫 번째 요소 제거
+pets.shift();
+console.log(pets);
+
+// unshift(..items)  : 배열에 맨 첫번째 데이터 추가
+pets.unshift('짝짝이');
+
+console.log(pets);
+
