77eef5ec258ad7405189b563998f820076fc79eb
kimjiseung97 <126743072+kimjiseung97@users.noreply.github.com>
2023-03-22 15:44:11 +0900

    콜백함수 연습

--- 5. ES6/콜백함수 연습.js
+++ 5. ES6/콜백함수 연습.js
@@ -1,32 +1,32 @@
-const userList = [{
-        account: 'abc1234',
-        userName: '대길이',
-        job: '추노',
-        address: '서울',
-        hobbys: ['수영', '축구', '테니스']
-    },
-    {
-        account: 'banana',
-        userName: '빠나나',
-        job: '과일',
-        address: '서울',
-        hobbys: ['푸드파이팅', '테니스']
-    },
-    {
-        account: 'park1234',
-        userName: '주차왕',
-        job: '발렛파킹',
-        address: '경기',
-        hobbys: ['족구', '축구', '테니스', '영화감상']
-    },
-    {
-        account: 'fire',
-        userName: '불꽃남자카리스마',
-        job: '게이머',
-        address: '서울',
-        hobbys: ['독서', '테니스']
-    },
-];
+// const userList = [{
+//         account: 'abc1234',
+//         userName: '대길이',
+//         job: '추노',
+//         address: '서울',
+//         hobbys: ['수영', '축구', '테니스']
+//     },
+//     {
+//         account: 'banana',
+//         userName: '빠나나',
+//         job: '과일',
+//         address: '서울',
+//         hobbys: ['푸드파이팅', '테니스']
+//     },
+//     {
+//         account: 'park1234',
+//         userName: '주차왕',
+//         job: '발렛파킹',
+//         address: '경기',
+//         hobbys: ['족구', '축구', '테니스', '영화감상']
+//     },
+//     {
+//         account: 'fire',
+//         userName: '불꽃남자카리스마',
+//         job: '게이머',
+//         address: '서울',
+//         hobbys: ['독서', '테니스']
+//     },
+// ];
 
 
 //회원목록에서 회원정보를 출력하는 함수
@@ -44,7 +44,7 @@ const userList = [{
 //특정 필터조건에 의해 필터링하는 함수
 
 function filter(condition) {
-
+    
     //새로운 배열 생성
     const fillteredArray = [];
     for (const user of userList) {
